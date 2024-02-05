6ca9e6e74aa7f241d195390487dd82e6004f5bc7
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:04:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-union-45912",
+  url: "https://yellow-union-45912.botics.co",
+  api: "https://yellow-union-45912.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
